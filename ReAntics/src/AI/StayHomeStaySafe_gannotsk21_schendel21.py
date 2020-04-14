6875083aeb923f9c9f53df8ea5947be352c11304
Alex Schendel <lexden.s@gmail.com>
2020-04-14 15:32:05 -0700

    Add TD-Learning Agent. Stores states

---
+++ ReAntics/src/AI/StayHomeStaySafe_gannotsk21_schendel21.py
@@ -0,0 +1,235 @@
+import random
+import sys
+sys.path.append("..")  #so other modules can be found in parent dir
+from Player import *
+from Constants import *
+from Construction import CONSTR_STATS
+from Ant import UNIT_STATS
+from Move import Move
+from GameState import *
+from AIPlayerUtils import *
+
+
+##
+#AIPlayer
+#Description: The responsbility of this class is to interact with the game by
+#deciding a valid move based on a given game state. This class has methods that
+#will be implemented by students in Dr. Nuxoll's AI course.
+#
+#Variables:
+#   playerId - The id of the player.
+##
+class AIPlayer(Player):
+
+    #__init__
+    #Description: Creates a new Player
+    #
+    #Parameters:
+    #   inputPlayerId - The id to give the new player (int)
+    #   cpy           - whether the player is a copy (when playing itself)
+    ##
+    def __init__(self, inputPlayerId):
+        super(AIPlayer,self).__init__(inputPlayerId, "Stay Home Stay Safe")
+        self.states = {}
+        self.previousStates = []
+    
+    ##
+    #getPlacement
+    #
+    #Description: called during setup phase for each Construction that
+    #   must be placed by the player.  These items are: 1 Anthill on
+    #   the player's side; 1 tunnel on player's side; 9 grass on the
+    #   player's side; and 2 food on the enemy's side.
+    #
+    #Parameters:
+    #   construction - the Construction to be placed.
+    #   currentState - the state of the game at this point in time.
+    #
+    #Return: The coordinates of where the construction is to be placed
+    ##
+    def getPlacement(self, currentState):
+        numToPlace = 0
+        #implemented by students to return their next move
+        if currentState.phase == SETUP_PHASE_1:    #stuff on my side
+            numToPlace = 11
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    #Choose any x location
+                    x = random.randint(0, 9)
+                    #Choose any y location on your side of the board
+                    y = random.randint(0, 3)
+                    #Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        elif currentState.phase == SETUP_PHASE_2:   #stuff on foe's side
+            numToPlace = 2
+            moves = []
+            for i in range(0, numToPlace):
+                move = None
+                while move == None:
+                    #Choose any x location
+                    x = random.randint(0, 9)
+                    #Choose any y location on enemy side of the board
+                    y = random.randint(6, 9)
+                    #Set the move if this space is empty
+                    if currentState.board[x][y].constr == None and (x, y) not in moves:
+                        move = (x, y)
+                        #Just need to make the space non-empty. So I threw whatever I felt like in there.
+                        currentState.board[x][y].constr == True
+                moves.append(move)
+            return moves
+        else:
+            return [(0, 0)]
+    
+    ##
+    #getMove
+    #Description: Gets the next move from the Player.
+    #
+    #Parameters:
+    #   currentState - The state of the current game waiting for the player's move (GameState)
+    #
+    #Return: The Move to be made
+    ##
+    def getMove(self, currentState):
+        moves = listAllLegalMoves(currentState)
+        selectedMove = moves[random.randint(0,len(moves) - 1)];
+        self.previousStates.append(self.categorizeState(currentState))
+        #don't do a build move if there are already 3+ ants
+        numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
+        while (selectedMove.moveType == BUILD and numAnts >= 3):
+            selectedMove = moves[random.randint(0,len(moves) - 1)];
+            
+        return selectedMove
+    
+    ##
+    #getAttack
+    #Description: Gets the attack to be made from the Player
+    #
+    #Parameters:
+    #   currentState - A clone of the current state (GameState)
+    #   attackingAnt - The ant currently making the attack (Ant)
+    #   enemyLocation - The Locations of the Enemies that can be attacked (Location[])
+    ##
+    def getAttack(self, currentState, attackingAnt, enemyLocations):
+        #Attack a random enemy.
+        return enemyLocations[random.randint(0, len(enemyLocations) - 1)]
+
+    ##
+    #registerWin
+    #
+    # This agent doens't learn
+    #
+    def registerWin(self, hasWon):
+        reward = -1
+        if hasWon:
+            reward = 1
+        print(self.previousStates)
+        self.previousStates = []
+
+
+    ##
+    #utility
+    #Description: Uses a gene to weight differnet values and give a state of the game a score
+    # 
+    #Parameters:
+    #   currentState - The current state (GameState)
+    #   gene         - A clone of the gene (a list of weights) that will be
+    #                 used to calculate the score of the current state
+    ##
+    def categorizeState(self, currentState):
+        me = currentState.whoseTurn
+        enemy = abs(me - 1)
+        myInv = getCurrPlayerInventory(currentState)
+        myFood = myInv.foodCount
+        enemyInv = getEnemyInv(self, currentState)
+        enemyFood = enemyInv.foodCount
+        tunnels = getConstrList(currentState, types=(TUNNEL,))
+        myTunnel = tunnels[1] if (tunnels[0].coords[1] > 5) else tunnels[0]
+        enemyTunnel = tunnels[0] if (myTunnel is tunnels[1]) else tunnels[1]
+        hills = getConstrList(currentState, types=(ANTHILL,))
+        myHill = hills[1] if (hills[0].coords[1] > 5) else hills[0]
+        enemyHill = hills[1] if (myHill is hills[0]) else hills[0]
+        myQueen = myInv.getQueen()
+        enemyQueen = enemyInv.getQueen()
+
+        foods = getConstrList(currentState, None, (FOOD,))
+
+        myWorkers = getAntList(currentState, me, (WORKER,))
+        myOffense = getAntList(currentState, me, (SOLDIER, DRONE))
+        enemyWorkers = getAntList(currentState, enemy, (WORKER,))
+        enemyOffense = getAntList(currentState, enemy, (SOLDIER, DRONE, R_SOLDIER))
+
+        score = ""
+        score += str(myFood)#0: food count
+        score += ','
+        score += str(myQueen.health)#1: queen health
+        score += ','
+        score += str(myHill.captureHealth)#2: anthill health
+        score += ','
+        score += '1' if len(myWorkers) == 2 else '0'#4: on/off based on two workers
+        score += ','
+        #3: utility of worker to target
+        tempScore = 0
+        if len(myWorkers) > 0:
+            for worker in myWorkers:
+                if worker.carrying: # if carrying go to hill/tunnel
+                    tempScore += 2
+                    distanceToTunnel = approxDist(worker.coords, myTunnel.coords)
+                    distanceToHill = approxDist(worker.coords, myHill.coords)
+                    dist = min(distanceToHill, distanceToTunnel)
+                    if dist <= 3:
+                        tempScore += 1
+                else: # if not carrying go to food
+                    dist = 100
+                    for food in foods:
+                        temp = approxDist(worker.coords, food.coords)
+                    if temp < dist:
+                        dist = temp
+                    if dist <= 3:
+                        tempScore += 1
+            score += str(tempScore)
+        else:
+            score += '0'
+        score += ','
+        #5: average distance from our offense to enemy queen
+        #6: average distance from our offense to the first enemy worker
+        #7: average distance from our offense to enemy anthill
+        queenDist = 0
+        workerDist = 0
+        anthillDist = 0
+        if len(myOffense) > 0:
+            for ant in myOffense:
+                queenDist += approxDist(ant.coords, enemyQueen.coords)
+            if len(enemyWorkers) > 0:
+                workerDist += approxDist(ant.coords, enemyWorkers[0].coords)
+            anthillDist += approxDist(ant.coords, enemyHill.coords)
+            score += str(queenDist//len(myOffense))
+            score += ','
+            score += str(workerDist//len(myOffense))
+            score += ','
+            score += str(anthillDist//len(myOffense))
+            score += ','
+        else:
+            score += "0,0,0,"
+        #8: average distance from enemy offense to our queen
+        #9: average distance from enemy offense to our anthill
+        queenDist = 0
+        anthillDist = 0
+        if len(enemyOffense) > 0:
+            for ant in enemyOffense:
+                queenDist += approxDist(ant.coords, myQueen.coords)
+                anthillDist += approxDist(ant.coords, myHill.coords)
+            score += str(queenDist//len(enemyOffense))
+            score += ','
+            score += str(anthillDist//len(enemyOffense))
+        else:
+            score += '0,0'
+        
+        # print(score)
+        return score
