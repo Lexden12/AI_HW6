882f8dfcb226c68a21fb5143e72e655ad2483ea3
Alex Schendel <lexden.s@gmail.com>
2020-04-17 10:45:47 -0700

    Implement saving/loading state space and decaying explore heuristic. Start testing

--- ReAntics/src/AI/StayHomeStaySafe_gannotsk21_schendel21.py
+++ ReAntics/src/AI/StayHomeStaySafe_gannotsk21_schendel21.py
@@ -35,7 +35,13 @@ class AIPlayer(Player):
         self.alpha = .1
         self.discount = .9
         # e = probability that we're gonna choose a random state (0-100)
-        # self.e = 100
+        self.eDecay = 0.999
+        self.e = 100
+        self.eMin = 10
+
+        self.fname = "gannotsk21_schendel21_states.txt"
+        self.loadStateSpace('../'+self.fname)
+        print(len(self.states))
 
     ##
     #getPlacement
@@ -104,22 +110,23 @@ class AIPlayer(Player):
         moves = listAllLegalMoves(currentState)
         selectedMove = moves[random.randint(0,len(moves) - 1)]
 
-        # Idea for explore Vs exploit
-        # # choosing next better state
-        # if random.randint(0, 100) > self.e:
-        #     # choose based on the saved utilities
-        #     for move in moves:
-        #         if self.states.get(self.categorizeState(getNextStateAdversarial(currentState, selectedMove))) == None:
-        #             continue
-        #         elif self.states.get(self.categorizeState(getNextStateAdversarial(currentState, move))) == None:
-        #             continue
-        #         elif self.states[self.categorizeState(getNextStateAdversarial(currentState, selectedMove))] <self.states[self.categorizeState(getNextStateAdversarial(currentState, move))]:
-        #             selectedMove = move
-        # self.e -= 1
+        #Idea for explore Vs exploit
+        # choosing next better state
+        if random.randint(0, 100) > self.e:
+            # choose based on the saved utilities
+            for move in moves:
+                if self.states.get(self.categorizeState(getNextStateAdversarial(currentState, selectedMove))) == None:
+                    continue
+                elif self.states.get(self.categorizeState(getNextStateAdversarial(currentState, move))) == None:
+                    continue
+                elif self.states[self.categorizeState(getNextStateAdversarial(currentState, selectedMove))] <self.states[self.categorizeState(getNextStateAdversarial(currentState, move))]:
+                    selectedMove = move
+        if self.e > self.eMin:
+            self.e *= self.eDecay
 
-        #don't do a build move if there are already 3+ ants
+        # #don't do a build move if there are already 3+ ants
         numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
-        while (selectedMove.moveType == BUILD and numAnts >= 3):
+        while (selectedMove.moveType == BUILD and numAnts >= 5):
             selectedMove = moves[random.randint(0,len(moves) - 1)];
 
         # get next state
@@ -153,6 +160,23 @@ class AIPlayer(Player):
         if hasWon:
             reward = 1
         self.calculations(self.previousStates, reward)
+        self.saveStateSpace(self.fname)
+
+    def saveStateSpace(self, path):
+        f = open(path, "w")
+        f.write("".join("{}: {}, ".format(k, v) for k, v in self.states.items()))
+        f.close()
+
+    def loadStateSpace(self, path):
+        f = open(path, "r")
+        self.states = {}
+        dictionary = f.read()
+        for entry in dictionary.split(', ')[0:-1]:
+            pair = entry.split(': ')
+            state = pair[0]
+            weight = pair[1]
+            self.states[state] = float(weight)
+        f.close()
 
     ##
     #calculations
@@ -170,7 +194,8 @@ class AIPlayer(Player):
             reward = hasWon
 
         #look up utility in dictionary
-        utility = self.states.setdefault(self.categorizeState(self.previousStates), 0)
+        category = self.categorizeState(self.previousStates)
+        utility = self.states.setdefault(category, 0)
         nextStateUtility = self.states.setdefault(self.categorizeState(nextState), 0)
 
         #Equation that I'm following => U(s)=U(s)+α⋅[R(s)+γ⋅U(s')−U(s)]
@@ -183,7 +208,7 @@ class AIPlayer(Player):
         # print("TDequation = " + str(TDequation))
 
         #save into dictionary
-        self.states[self.previousStates] = TDequation
+        self.states[category] = TDequation
 
 
     ##
@@ -218,13 +243,13 @@ class AIPlayer(Player):
 
         score = ""
         score += str(myFood)#0: food count
-        score += ','
+        score += ";"
         score += str(myQueen.health)#1: queen health
-        score += ','
+        score += ";"
         score += str(myHill.captureHealth)#2: anthill health
-        score += ','
-        score += '1' if len(myWorkers) == 2 else '0'#4: on/off based on two workers
-        score += ','
+        score += ";"
+        score += "1" if len(myWorkers) == 2 else "0"#4: on/off based on two workers
+        score += ";"
         #3: utility of worker to target
         tempScore = 0
         if len(myWorkers) > 0:
@@ -246,8 +271,8 @@ class AIPlayer(Player):
                         tempScore += 1
             score += str(tempScore)
         else:
-            score += '0'
-        score += ','
+            score += "0"
+        score += ";"
         #5: average distance from our offense to enemy queen
         #6: average distance from our offense to the first enemy worker
         #7: average distance from our offense to enemy anthill
@@ -256,31 +281,32 @@ class AIPlayer(Player):
         anthillDist = 0
         if len(myOffense) > 0:
             for ant in myOffense:
-                queenDist += approxDist(ant.coords, enemyQueen.coords)
-            if len(enemyWorkers) > 0:
-                workerDist += approxDist(ant.coords, enemyWorkers[0].coords)
+                if enemyQueen is not None:
+                    queenDist += approxDist(ant.coords, enemyQueen.coords)
+                if len(enemyWorkers) > 0:
+                    workerDist += approxDist(ant.coords, enemyWorkers[0].coords)
             anthillDist += approxDist(ant.coords, enemyHill.coords)
             score += str(queenDist//len(myOffense))
-            score += ','
+            score += ";"
             score += str(workerDist//len(myOffense))
-            score += ','
+            score += ";"
             score += str(anthillDist//len(myOffense))
-            score += ','
+            score += ";"
         else:
-            score += "0,0,0,"
+            score += "0;0;0;"
         #8: average distance from enemy offense to our queen
         #9: average distance from enemy offense to our anthill
         queenDist = 0
         anthillDist = 0
         if len(enemyOffense) > 0:
             for ant in enemyOffense:
-                queenDist += approxDist(ant.coords, myQueen.coords)
+                if myQueen is not None:
+                    queenDist += approxDist(ant.coords, myQueen.coords)
                 anthillDist += approxDist(ant.coords, myHill.coords)
             score += str(queenDist//len(enemyOffense))
-            score += ','
+            score += ";"
             score += str(anthillDist//len(enemyOffense))
         else:
-            score += '0,0'
+            score += "0;0"
 
-        # print(score)
         return score
